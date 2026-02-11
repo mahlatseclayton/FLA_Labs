617275b42db29ba7b91781b14ffacfc8df9e8d5e
mahlatseclayton <mahlatseclayton1@gmail.com>
2026-02-11 23:04:45 +0200

    lab c

---
+++ lab1_b.py
@@ -0,0 +1,9 @@
+# b) takes as input an integer 𝑘, and outputs the binary string ⟨𝑘⟩ as from the notes, and a binary
+# type identifier as an integer
+# • 𝑃(⟨𝑘⟩10
+# ) = ⟨ℤ⟩#⟨𝑘⟩
+int_id=1 #identifier for integers
+print("enter an Integer!")
+int_input=int(input())
+print(str(int_id)+"#"+bin(int_input))
+
---
+++ lab1_c.py
@@ -0,0 +1,19 @@
+# b) takes as input an integer 𝑘, and outputs the binary string ⟨𝑘⟩ as from the notes, and a binary
+# type identifier as an integer
+# • 𝑃(⟨𝑘⟩10
+# ) = ⟨ℤ⟩#⟨𝑘⟩
+# c) repeats the previous question but for the rationals ℚ, a string Σ
+# ∗
+# , a symbol Σ
+# • Given that we are now using # to seperate our list, and our type hints, how can we represent
+# both simultaneously and without a collision of meaning?
+from fractions import Fraction
+int_id=1 #identifier for integers
+rat_id=2 #identifier for rationals
+print("enter a rational number!")
+x=input()
+rat_input=Fraction(x)
+print(str(rat_id)+"#"+bin(rat_input.numerator)+"#"+bin(rat_input.denominator))
+
+
+
